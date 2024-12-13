9d4b72d6d8a5c37d70a2935fbefedcde30f017e9
zermpinos <130582198+zermpinos@users.noreply.github.com>
2024-12-13 16:29:09 +0200

    Uploaded the wrong day, mb

--- Day 5 - Print Queue/printqueue.py
+++ Day 5 - Print Queue/printqueue.py
@@ -1,23 +1,40 @@
+import re
+
+rules, part1, part2 = [], [], []
+
 with open("input.txt") as file:
-    data = file.read().split("mul(")
+    for line in file:
+        data = line.rstrip()
 
-multiplication1, multiplication2 = 0, 0
+        if "|" in data:
+            rules.append(data.split("|"))
 
-activate = True # Flag variable to track whether we are in the 'do()' or 'don't()' sequence
+        elif "," in data:
+            passing = True
+            for rule in rules:
+                if re.search(r"" + rule[1] + ".+" + rule[0], data):
+                    passing = False
+                    break
 
-for sequence in data:
-    sequence_parse = sequence.split(",", 1)
-    value1 = sequence_parse[0]
+            if passing:
+                result= data.split(",")
+                part1.append(int(result[len(result) // 2]))
 
-    if value1.isdigit():
-        value2 = sequence_parse[1].split(")")[0]
-        if value2.isdigit():
-            multiplication1 += int(value1)*int(value2)
-            if activate: multiplication2 += int(value1)*int(value2)
+            else:
+                while True:
+                    passing = True
+                    for rule in rules:
+                        match = re.search(r"(" + rule[1] + r").+(" + rule[0] + r")", data)
+                        if match:
+                            passing = False
+                            data_array = data.split(",")
+                            temporary = data_array[match.span(1)[0] // 3]
+                            data_array[match.span(1)[0] // 3] = data_array[match.span(2)[0] // 3]
+                            data_array[match.span(2)[0] // 3] = temporary
+                            data = ",".join(data_array)
+                    if passing:
+                        break
+                result= data.split(",")
+                part2.append(int(result[len(result) // 2]))
 
-    # Check which direction of comparison is more frequent
-    if sequence.find("don't()") > sequence.find("do()"): activate = False
-    if sequence.find("do()") > sequence.find("don't()"): activate = True
-
-
-print(multiplication1, multiplication2)
+print(sum(part1), sum(part2))
